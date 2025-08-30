998acaad2fef56bdcb192be8556b13f9747d4ac9
mdnestor <mdnest01@gmail.com>
2025-08-30 16:23:56 -0400

    update readme

--- script.py
+++ script.py
@@ -51,7 +51,7 @@ Some basic point-set topology in Lean for fun.
 
 The main file is [`basic.lean`](./basic_topology/basic.lean) and there is also an automatically generated file called [`exercise.lean`](./basic_topology/exercise.lean) with proofs of theorems removed if you want to try yourself :)
 
-I also copied the code into clickable links to the Lean 4 Web server. Love that place
+I also copied the code into clickable links to the Lean 4 Web server.
 - [Link to main file](https://live.lean-lang.org/#codez={basic_parsed}) (slow)
 - [Link to exercises](https://live.lean-lang.org/#codez={exercises_parsed})
 
